510a170b9b7662ef889ff165c6a29aa1b0c5724b
Artem <artctvcode@gmail.com>
2022-12-14 17:19:14 +0500

    remove extra routes

--- engineering/api/routes.py
+++ engineering/api/routes.py
@@ -1,21 +1,9 @@
-from fastapi import APIRouter, Depends
-from redis import Redis
-from rq import Queue
-from config import settings
-from .dependencies import get_redis, get_queue
+from fastapi import APIRouter
 
 
 router: APIRouter = APIRouter()
 
 
-@router.get("/predict")
-def main(q: Queue = Depends(get_queue)):
-    q.enqueue(settings.worker.tasks["some_func"], 30, job_id="my_id")
-    # сохарнить картинку в файл в папку
-    return {"message": "ok"}
-
-
-@router.get("/retrive/{job_id}")
-def main(redis: Redis = Depends(get_redis)):
-    redis.setex("key", 10, "value")
+@router.get("/")
+def main():
     return {"message": "ok"}
