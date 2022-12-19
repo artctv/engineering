631958184698ed1232f43d413343096a41022d82
KalimullinaA <kalimullina.alina2009@yandex.ru>
2022-12-19 20:26:28 +0500

    Test file. Model calculation

--- engineering/worker/tasks.py
+++ engineering/worker/tasks.py
@@ -1,3 +1,28 @@
+from transformers import ViTFeatureExtractor, ViTForImageClassification
+from PIL import Image
+import os
+import requests
+
+# ссылка на папку
+path = 'images/sample.jpg'
+
+# Открыть, Закрыть картинку по пути
+with Image.open(path) as image:
+
+    # расчет модели
+    feature_extractor = ViTFeatureExtractor.from_pretrained('google/vit-base-patch16-224')
+    model = ViTForImageClassification.from_pretrained('google/vit-base-patch16-224')
+    inputs = feature_extractor(images=image, return_tensors="pt")
+    outputs = model(**inputs)
+    logits = outputs.logits
+    #os.remove('/images/sample.jpg') #удалить картинку из папки
+
+
+# Предсказание
+predicted_class_idx = logits.argmax(-1).item()
+print("Predicted class:", model.config.id2label[predicted_class_idx])
+
+
 """
     Примеры кода для сохранения
 
@@ -15,11 +40,9 @@
     Статусы таски
 """
 
-import time
-
-
-def some_func(timeout):
-    time.sleep(timeout)
-    return 'kek'
-
-
+# import time
+#
+#
+# def some_func(timeout):
+#     time.sleep(timeout)
+#     return 'kek'
