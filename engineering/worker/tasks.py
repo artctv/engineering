c6069dca592c21ac380326a96be34584c077752e
KalimullinaA <kalimullina.alina2009@yandex.ru>
2022-12-22 21:22:28 +0500

    Added function with result record

--- engineering/worker/tasks.py
+++ engineering/worker/tasks.py
@@ -8,41 +8,22 @@ path = 'images/sample.jpg'
 
 # Открыть, Закрыть картинку по пути
 with Image.open(path) as image:
-
     # расчет модели
     feature_extractor = ViTFeatureExtractor.from_pretrained('google/vit-base-patch16-224')
     model = ViTForImageClassification.from_pretrained('google/vit-base-patch16-224')
     inputs = feature_extractor(images=image, return_tensors="pt")
     outputs = model(**inputs)
     logits = outputs.logits
-    #os.remove('/images/sample.jpg') #удалить картинку из папки
-
+    os.remove('/images/sample.jpg') #удалить картинку из папки
 
 # Предсказание
 predicted_class_idx = logits.argmax(-1).item()
-print("Predicted class:", model.config.id2label[predicted_class_idx])
+func_result = model.config.id2label[predicted_class_idx]
 
 
-"""
-    Примеры кода для сохранения
+#Результат
+def result():
+    print("Predicted class:", func_result)
 
-    # Коннект к редис внутри job
-    from rq import get_current_job
-    from rq.job import Job
-    _job: Job = get_current_job()
-    print(_job.connection, type(_job.connection))
 
-    # Получение таски, её резльтат и её статус
-    job = Job.fetch(job_id)
-    print(job.result, job.get_status())
 
-    from rq.job import JobStatus
-    Статусы таски
-"""
-
-# import time
-#
-#
-# def some_func(timeout):
-#     time.sleep(timeout)
-#     return 'kek'
