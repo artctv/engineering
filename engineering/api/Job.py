ad95258aa082361350cf55baa1cf226bfd5084e4
Elvina8 <Kalimullina.elvina2016@yandex.ru>
2022-12-24 20:45:35 +0500

    Model result test
    Status processing

---
+++ engineering/api/Job.py
@@ -0,0 +1,9 @@
+class JobStatus(str, Enum):
+    Queued = 'queued'
+    Finished = 'finished'
+    Failed = 'failed'
+    Started = 'started'
+    Deferred = 'deferred'
+    Scheduled = 'scheduled'
+    Stopped = 'stopped'
+    Canceled = 'canceled'
--- engineering/api/routes.py
+++ engineering/api/routes.py
@@ -1,9 +1,39 @@
 from fastapi import APIRouter
-
+from uuid import UUID
+from engineering.api.dependencies import get_queue
+from rq.job import JobStatus
+from rq import Queue
+from rq.exceptions import NoSuchJobError
+from .dependencies import get_queue
+from fastapi import Depends
 
 router: APIRouter = APIRouter()
 
-
-@router.get("/")
-def main():
+#Получение модели
+@router.get("/{uuid}")
+def main(uuid: UUID):
     return {"message": "ok"}
+
+
+
+
+
+
+
+
+#Обработка статусов
+def main(q: Queue = Depends(get_queue)):
+    print(JobStatus.Failed)
+    status = "canceled"
+    if status == JobStatus.Canceled:
+        return("Ваш запрос был отменен.Повторите попытку позже")
+    elif status == JobStatus.Queued:
+        return("Ваш запрос в очереди,ожидайте")
+    elif status == JobStatus.Finished:
+        return("Ваш запрос обработан")
+    elif status == JobStatus.Started:
+        return ("Ваш запрос в обработке")
+    elif status == JobStatus.Deferred:
+        return ("Ваш запрос отложен, повторите попытку позже")
+
+        
