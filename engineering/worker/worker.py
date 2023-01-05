5bc2790eaa441a0a8e5eead4664309b7bd3840eb
Artem <artctvcode@gmail.com>
2023-01-05 22:36:57 +0500

    add os specific workers

--- engineering/worker/worker.py
+++ engineering/worker/worker.py
@@ -2,10 +2,12 @@ from typing import Any
 from rq import Connection, Worker
 from redis import Redis
 from transformers import ViTFeatureExtractor, ViTForImageClassification
+from rq_win import WindowsWorker
+import platform
 from config import settings
 
 
-class ExtendedWorker(Worker):
+class BaseWorker:
     _feature_extractor: Any
     _model: Any
 
@@ -14,6 +16,17 @@ class ExtendedWorker(Worker):
         self._model = ViTForImageClassification.from_pretrained('google/vit-base-patch16-224')
         super().__init__(*args, **kwargs)
 
+
+class ExtendedWorker(BaseWorker, Worker):
+    def execute_job(self, job, queue):
+        job.args = (job.args[0], self._feature_extractor, self._model)
+        return self.perform_job(job, queue)
+
+    def work(self, *args, **kwargs):
+        return super().work(*args, **kwargs)
+
+
+class ExtendedWindowWorker(BaseWorker, WindowsWorker):
     def execute_job(self, job, queue):
         job.args = (job.args[0], self._feature_extractor, self._model)
         return self.perform_job(job, queue)
@@ -29,7 +42,11 @@ def run():
         db=settings.redis.db,
         password=settings.redis.password
     )
-    with Connection(connection=redis):
+    if platform.system() == "Windows":
+        worker: Worker = ExtendedWindowWorker(settings.worker.queues)
+    else:
         worker: Worker = ExtendedWorker(settings.worker.queues)
+
+    with Connection(connection=redis):
         worker.work()
 
