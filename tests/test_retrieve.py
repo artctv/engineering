3022ade3b3d6d460ccc2993c9aa7831a651c549f
Elvina8 <Kalimullina.elvina2016@yandex.ru>
2023-01-10 13:27:46 +0500

    uuid correctness check

--- tests/test_retrieve.py
+++ tests/test_retrieve.py
@@ -1,5 +1,7 @@
+import requests
 from fastapi import FastAPI
 from fastapi.testclient import TestClient
+from uuid import uuid4
 
 app = FastAPI()
 client = TestClient(app)
@@ -14,4 +16,14 @@ def test_read_main():
     assert response.status_code == 200
     assert response.json() == {"message": "Все хорошо"}
     print(response.json())
+
+class TestUuid:
+    def test_get_job_by_id(self, connection = None):
+        response = requests.get("https://uuid", json=connection)
+        assert response.status_code == 201
+        assert response.json().get('uuid')
+        assert isinstance(response.json().get('uuid'), int)
+        print(response.text)
+
+
 
