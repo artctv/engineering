cd11df13c0cec7e3a5927b3fd6501d0230e773b4
Elvina8 <Kalimullina.elvina2016@yandex.ru>
2023-01-12 01:17:43 +0500

    feature replacement

--- tests/mocks.py
+++ tests/mocks.py
@@ -1,3 +1,11 @@
+class c_status:
+    def get_status(self, *args, **kwargs):
+        # print(args, kwargs)
+        return 'finished'
+
+
+
+
 
 
 def overrided_queue():
@@ -6,3 +14,8 @@ def overrided_queue():
 
 def overrided_delay():
     pass
+def overrided_get_job_by_id():
+    object_job = c_status()
+    return object_job
+
+
--- tests/test_retrieve.py
+++ tests/test_retrieve.py
@@ -3,27 +3,23 @@ from fastapi import FastAPI
 from fastapi.testclient import TestClient
 from uuid import uuid4
 
-app = FastAPI()
-client = TestClient(app)
-
-@app.get("/retrieve")
-def root():
-    return{"message":"Все хорошо"}
+from pytest_mock import MockFixture
 
 
-def test_read_main():
-    response = client.get("/retrieve")
+
+
+def test_read_main(client):
+    uuid = str(uuid4())
+    response = client.get("/retrieve/{}".format(uuid))
     assert response.status_code == 200
-    assert response.json() == {"message": "Все хорошо"}
-    print(response.json())
+    result = response.json()
+    assert "status" in result
+    assert "result" in result
 
-class TestUuid:
-    def test_get_job_by_id(self, connection = None):
-        response = requests.get("https://uuid", json=connection)
-        assert response.status_code == 201
-        assert response.json().get('uuid')
-        assert isinstance(response.json().get('uuid'), int)
-        print(response.text)
+
+
+  
+
 
 
 
